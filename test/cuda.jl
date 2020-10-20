0c5d4ea1e394ed199ae859d2e5f1a2efb0eef313
Łukasz Pawela <lukasz.pawela@gmail.com>
2020-10-21 00:26:04 +0200

    fix cuda tests

--- test/cuda.jl
+++ test/cuda.jl
@@ -21,8 +21,6 @@
         ψ = randn(MPS{CuArray{Float32, 3}}, sites, 10, 2)
         H = randn(MPO{CuArray{Float32, 4}}, sites, 10, 2)
 
-        @test typeof((H*H).tensors[1]) <: CuArray
         @test typeof((H*ψ).tensors[1]) <: CuArray
-        @test typeof((ψ' * H).parent.tensors[1]) <: CuArray
     end
 end
