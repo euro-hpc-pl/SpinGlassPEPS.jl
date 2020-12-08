f43eacccbc0be9b4a69eeb1ac490cd86664db7bf
kdomino <kdomino@iitis.pl>
2020-12-08 13:38:40 +0100

    remove generic linear algebra

--- src/SpinGlassPEPS.jl
+++ src/SpinGlassPEPS.jl
@@ -6,7 +6,6 @@ module SpinGlassPEPS
     using LightGraphs
     using MetaGraphs
     using CSV
-    using GenericLinearAlgebra
 
     using DocStringExtensions
     const product = Iterators.product
