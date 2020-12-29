147573460fbe12acd63886e06a0a019886964450
Bartłomiej Gardas <bartek.gardas@gmail.com>
2020-12-29 22:21:37 +0100

    add changes MPS/MPO from graph concept

--- src/PEPS.jl
+++ src/PEPS.jl
@@ -35,3 +35,34 @@ function generate_tensor(ng::NetworkGraph, v::Int, w::Int)
     end
     tensor
 end
+
+#=
+function MPO(fg::MetaDiGraph, dim::Symbol=:r, i::Int; T::DataType=Float64)
+    @assert dir ∈ (:r, :c)
+
+    m, n = size(fg)
+    idx = LinearIndices((1:m, 1:n))
+    chain = dim == :r ? fg[idx[:, i]] : fg[idx[i, :]] 
+
+    ψ = MPO(T, length(chain))
+
+    for (j, v) ∈ enumerate(chain)
+        ψ[j] = PepsTensor(fg, v).tensor
+    end
+    ψ
+end
+
+function MPS(fg::MetaDiGraph, which::Symbol=:d; T::DataType=Float64)
+    @assert which ∈ (:l, :r, :u, :d)
+
+    #ϕ = MPO()
+
+    for (j, v) ∈ enumerate(_row(fg, 1))
+        ψ[j] = dropdims(PepsTensor(fg, v).tensor, dims=4)
+    end
+
+    # TBW 
+
+    ψ
+end
+=#
