48b101573e6bad1114418aa6699feedfe53e9c30
annamariadziubyna <73058800+annamariadziubyna@users.noreply.github.com>
2021-01-12 23:15:05 +0100

    change bond dim

--- test/PEPS.jl
+++ test/PEPS.jl
@@ -9,7 +9,7 @@ t = 3
 
 L = m * n * t
 
-bond_dimensions = [2, 2, 4, 4, 2, 2, 8]
+bond_dimensions = [2, 2, 8, 4, 2, 2, 8]
 
 instance = "$(@__DIR__)/instances/pathological/test_$(m)_$(n)_$(t).txt" 
 
@@ -25,22 +25,34 @@ fg = factor_graph(
     spectrum=full_spectrum,
 )
 
-#=
 for (bd, e) in zip(bond_dimensions, edges(fg))
     pl, en, pr = get_prop(fg, e, :split)
-
     println(e)
     println(size(pl), "   ", size(en),  "   ", size(pr))
-
+   #=
+    display(en)
+    println("-------------------")
+    println("-------------------")
+    display(pl)
+    println("-------------------")
+    println("-------------------")
+    display(pr)
+    println("-------------------")
+    println("-------------------")
+    println("-------------------")
+    =#
     isOK = min(size(en)...) == bd
-
+    
     @test isOK
     if !isOK
         println(min(size(en)...), " ", bd)
         display(en)
+        display(pl)
+        display(pr)
     end
+    
 end
-=#
+
 
 x, y = m, n
 
