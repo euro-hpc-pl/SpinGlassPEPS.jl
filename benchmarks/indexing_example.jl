57293b45600c2113c5eac0f5d3f228908f8c8ae1
kdomino <kdomino@iitis.pl>
2021-01-20 11:17:34 +0100

    indexing_example.jl with tests

--- benchmarks/indexing_example.jl
+++ benchmarks/indexing_example.jl
@@ -1,94 +1,132 @@
 using SpinGlassPEPS
 using MetaGraphs
 using LightGraphs
+using Test
 
-#      grid
-#     A1    |    A2
-#           |
-#   1 -- 3 -|- 5 -- 7
-#   |    |  |  |    |
-#   |    |  |  |    |
-#   2 -- 4 -|- 6 -- 8
-#           |
+@testset "test weather the solution of the tensor comply with the brute force" begin
 
-D = Dict{Tuple{Int64,Int64},Float64}()
-push!(D, (1,1) => 2.5)
-push!(D, (2,2) => 1.4)
-push!(D, (3,3) => 2.3)
-push!(D, (4,4) => 1.2)
-push!(D, (5,5) => -2.5)
-push!(D, (6,6) => -.5)
-push!(D, (7,7) => -.3)
-push!(D, (8,8) => -.2)
+    #      grid
+    #     A1    |    A2
+    #           |
+    #   1 -- 3 -|- 5 -- 7
+    #   |    |  |  |    |
+    #   |    |  |  |    |
+    #   2 -- 4 -|- 6 -- 8
+    #           |
 
-push!(D, (1,2) => 1.3)
-push!(D, (3,4) => -1.)
-push!(D, (5,6) => 1.1)
-push!(D, (7,8) => .1)
+    D = Dict{Tuple{Int64,Int64},Float64}()
+    push!(D, (1,1) => 2.5)
+    push!(D, (2,2) => 1.4)
+    push!(D, (3,3) => 2.3)
+    push!(D, (4,4) => 1.2)
+    push!(D, (5,5) => -2.5)
+    push!(D, (6,6) => -.5)
+    push!(D, (7,7) => -.3)
+    push!(D, (8,8) => -.2)
 
-push!(D, (1,3) => .8)
-push!(D, (3,5) => .5)
-push!(D, (5,7) => -1.)
+    push!(D, (1,2) => 1.3)
+    push!(D, (3,4) => -1.)
+    push!(D, (5,6) => 1.1)
+    push!(D, (7,8) => .1)
 
-push!(D, (2,4) => 1.7)
-push!(D, (4,6) => -1.5)
-push!(D, (6,8) => 1.2)
+    push!(D, (1,3) => .8)
+    push!(D, (3,5) => .5)
+    push!(D, (5,7) => -1.)
 
-m = 1
-n = 2
-t = 4
+    push!(D, (2,4) => 1.7)
+    push!(D, (4,6) => -1.5)
+    push!(D, (6,8) => 1.2)
 
-L = m * n * t
+    m = 1
+    n = 2
+    t = 4
 
-g_ising = ising_graph(D, L)
+    L = m * n * t
 
-update_cells!(
-  g_ising,
-  rule = square_lattice((m, 1, n, 1, t)),
-)
+    g_ising = ising_graph(D, L)
 
-fg = factor_graph(
-    g_ising,
-    energy=energy,
-    spectrum=full_spectrum,
-)
+    update_cells!(
+      g_ising,
+      rule = square_lattice((m, 1, n, 1, t)),
+    )
+
+    fg = factor_graph(
+        g_ising,
+        energy=energy,
+        spectrum=full_spectrum,
+    )
 
 
-#println([get_prop(fg, e, :edge) for e in edges(fg)])
+    #println([get_prop(fg, e, :edge) for e in edges(fg)])
 
-origin = :NW
-β = 2.
+    origin = :NW
+    β = 2.
 
-x, y = m, n
-peps = PepsNetwork(x, y, fg, β, origin)
-pp = PEPSRow(peps, 1)
-println(pp)
+    x, y = m, n
+    peps = PepsNetwork(x, y, fg, β, origin)
+    pp = PEPSRow(peps, 1)
 
-bf = brute_force(g_ising; num_states = 1)
-#println(bf.energies)
+    # brute force solution
+    bf = brute_force(g_ising; num_states = 1)
+    states = bf.states[1]
 
-states = bf.states[1]
+    sol_A1 = states[1:4]
+    sol_A2 = states[5:8]
 
-cell_A1 = states[1:4]
-cell_A1_left = states[3:4]
-cell_A2 = states[5:8]
-cell_A2_right = states[5:6]
 
-A2 = @state pp[2][cell_A1_left, 1, 1, 1, :]
+    # solutions from A1
+    # index 3 (right) and % (physical are not trivial)
+    A1 = pp[1][1,1,:,1,:]
 
-_, spins = findmax(A2)
+    # A2 traced
+    # index 1 (left is not trivial)
+    A2 = MPO(pp)[2][:,1,1,1]
 
-st = get_prop(fg, 2, :spectrum).states
+    # contraction of A1 with A2
+    #
+    #              .           .
+    #            .           .
+    #   A1 -- A2      =  A12
+    #
+    A12 = (transpose(A2)*A1)[1,:]
 
-println("ground state of A2 from PEPS at left index $(cell_A1_left) equals to = ", st[spins] )
-println("this should correspond to the ground state of A2 from brute force = ", cell_A2)
+    # maximal margianl probability
 
-println("............................")
+    _, spins = findmax(A12)
 
-for i in 1:4
-    A2p = @state pp[2][i, 1, 1, 1, :]
+    st = get_prop(fg, 1, :spectrum).states
 
+    # reading solution from energy numbering and comparison with brute force
+
+    @test st[spins] == sol_A1
+
+
+
+    println("grount state of A2 from brute force is ", sol_A2)
+
+    for i in 1:4
+        A2p = @state pp[2][i, 1, 1, 1, :]
+
+        _, spins_p = findmax(A2p)
+
+        println("at arbitrary left index $i ground state of A2 from PEPS is = ", st[spins_p])
+    end
+
+    # solutions from A2
+    # seting spins
+    A1_left = sol_A1[3:4]
+
+    println("left spins from A1", A1_left)
+
+    A2 = @state pp[2][A1_left, 1, 1, 1, :]
+    A2p = @state pp[2][2, 1, 1, 1, :]
+
+    _, spins = findmax(A2)
     _, spins_p = findmax(A2p)
 
-    println("at arbitrary left index $i ground state of A2 from PEPS is = ", st[spins_p])
+    st = get_prop(fg, 2, :spectrum).states
+
+    @test st[spins_p] == sol_A2
+    @test st[spins] == sol_A2
+
 end
