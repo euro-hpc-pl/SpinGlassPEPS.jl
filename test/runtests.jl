343f1650a8f93be8080b62b521e644143504724f
Łukasz Pawela <lukasz.pawela@gmail.com>
2021-01-24 14:06:05 +0100

    restore tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,19 +31,19 @@ if CUDA.functional() && CUDA.has_cutensor() && false
 end
 
 push!(my_tests,
-    #"base.jl",
-    #"contractions.jl",
-    #"compressions.jl",
-    #"ising.jl",
-    "indexing.jl"
-    #"spectrum.jl",
-    #"graph.jl",
-    #"PEPS.jl",
-    #"notation_tests.jl",
-    #"peps_tests.jl",
-    #"mps_tests.jl",
-    #"tests_full_graph.jl",
-    #"tests_on_data.jl"
+    "base.jl",
+    "contractions.jl",
+    "compressions.jl",
+    "ising.jl",
+    "indexing.jl",
+    "spectrum.jl",
+    "graph.jl",
+    "PEPS.jl",
+    "notation_tests.jl",
+    "peps_tests.jl",
+    "mps_tests.jl",
+    "tests_full_graph.jl",
+    "tests_on_data.jl"
 )
 
 for my_test in my_tests
