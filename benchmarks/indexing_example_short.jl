03e1edf6a0b3f1a47c379c288208509e14f0988d
kdomino <kdomino@iitis.pl>
2021-01-26 08:46:19 +0100

    correct tests

--- benchmarks/indexing_example_short.jl
+++ benchmarks/indexing_example_short.jl
@@ -28,6 +28,13 @@ using TensorCast
 
         g_ising = ising_graph(D, L)
 
+        # brute force solution
+        bf = brute_force(g_ising; num_states = 1)
+        states = bf.states[1]
+        sol_A1 = states[[1,2]]
+        sol_A2 = states[[3]]
+
+        #particular form of peps tensors
         update_cells!(
           g_ising,
           rule = square_lattice((m, 1, n, 1, t)),
@@ -40,19 +47,39 @@ using TensorCast
             spectrum = brute_force,
         )
 
-
         origin = :NW
         β = 1.
 
         x, y = m, n
         peps = PepsNetwork(x, y, fg, β, origin)
         pp = PEPSRow(peps, 1)
+        if D[(1, 2)] == 0.652
+            h1 = D[(1,1)]
+            h2 = D[(2,2)]
+            J12 = D[(1,2)]
+            J23 = D[(2,3)]
+            h3 = D[(3,3)]
 
-        # brute force solution
-        bf = brute_force(g_ising; num_states = 1)
-        states = bf.states[1]
-        sol_A1 = states[[1,2]]
-        sol_A2 = states[[3]]
+            println("matricised A1")
+            display(pp[1][1,1,:,1,:])
+            println()
+            #its explicite form including J23
+            A1ex = [exp(h1+h2-J12-J23) exp(h1-h2+J12+J23) exp(-h1+h2+J12-J23) exp(-h1-h2-J12+J23); exp(h1+h2-J12+J23) exp(h1-h2+J12-J23) exp(-h1+h2+J12+J23) exp(-h1-h2-J12-J23)]
+            println("A1 equal to its explicite form including J23")
+            println(pp[1][1,1,:,1,:] ≈ A1ex)
+            # but J23 should be in A2
+
+            println("matricised A2")
+            display(pp[2][:,1,1,1,:])
+            println("is explicite form do not include J23")
+            #but should
+            println(pp[2][:,1,1,1,:] ≈ [exp(h3) 0.; 0. exp(-h3)])
+            println("why A2 is diagonal")
+        end
+
+
+
+
 
         # peps solution
         Aa1 = pp[1]
