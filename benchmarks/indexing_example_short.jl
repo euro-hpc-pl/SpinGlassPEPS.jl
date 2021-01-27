7af8f02511f784a588a6dc3bd08e18290d2e8a44
kdomino <kdomino@iitis.pl>
2021-01-27 11:01:01 +0100

    tests are passing with higher beta

--- benchmarks/indexing_example_short.jl
+++ benchmarks/indexing_example_short.jl
@@ -15,7 +15,6 @@ using TensorCast
 
 
     D1 = Dict((1, 2) => 0.652,(2, 3) => 0.73,(3, 3) => 0.592,(2, 2) => 0.704,(1, 1) => 0.868)
-
     D2 = Dict((1, 2) => -0.9049,(2, 3) => 0.2838,(3, 3) => -0.7928,(2, 2) => 0.1208,(1, 1) => -0.3342)
 
     for D in [D1, D2]
@@ -25,7 +24,6 @@ using TensorCast
         t = 2
 
         L = m * n * t
-
         g_ising = ising_graph(D, L)
 
         # brute force solution
@@ -48,16 +46,18 @@ using TensorCast
         )
 
         origin = :NW
-        β = 1.
+        β = 2.
 
         x, y = m, n
         peps = PepsNetwork(x, y, fg, β, origin)
         pp = PEPSRow(peps, 1)
 
-        display(pp[1][1,1,:,1,:])
-        println()
-        display(pp[2][:,1,1,1,:])
-        println()
+        # the solution without cutting off
+        M1 = pp[1][1,1,:,1,:]
+        M2 = pp[2][:,1,1,1,:]
+        @reduce MM[a,b] |= sum(x) M1[x,a]*M2[x,b]
+
+        _, inds = findmax(MM)
 
         h1 = D[(1,1)]
         h2 = D[(2,2)]
@@ -66,54 +66,33 @@ using TensorCast
         h3 = D[(3,3)]
 
         if D[(1, 2)] == 0.652
-
             A1ex = reshape([exp(h1+h2-J12) 0. exp(-h1+h2+J12) 0.; 0. exp(h1-h2+J12) 0. exp(-h1-h2-J12)], (1,1,2,1,4))
-            @test A1ex ≈ pp[1]
-
             A2ex = reshape([exp(h3-J23) exp(-h3+J23); exp(h3+J23) exp(-h3-J23)],(2,1,1,1,2))
-            @test A2ex ≈ pp[2]
+            C = [exp(h1+h2-J12+h3-J23) exp(h1-h2+J12+h3+J23) exp(-h1+h2+J12+h3-J23) exp(-h1-h2-J12+h3+J23); exp(h1+h2-J12-h3+J23) exp(h1-h2+J12-h3-J23) exp(-h1+h2+J12-h3+J23) exp(-h1-h2-J12-h3-J23)]
         else
             A1ex = reshape([exp(-h1-h2-J12) 0. 0. exp(h1-h2+J12); 0. exp(h1+h2-J12) exp(-h1+h2+J12) 0.], (1,1,2,1,4))
-            display(A1ex[1,1,:,1,:])
-            println()
-            a = [1 2;3 4]
-
             A2ex = reshape([exp(-h3-J23) exp(h3+J23); exp(-h3+J23) exp(h3-J23)],(2,1,1,1,2))
-            display(A2ex[:,1,1,1,:])
-            println()
-
+            C = [exp(-h1-h2-J12-h3-J23) exp(h1+h2-J12-h3+J23) exp(-h1+h2+J12-h3+J23) exp(h1-h2+J12-h3-J23); exp(-h1-h2-J12+h3+J23) exp(h1+h2-J12+h3-J23) exp(-h1+h2+J12+h3-J23) exp(h1-h2+J12+h3+J23)]
         end
-        # the solution without cutting off
-        M1 = pp[1][1,1,:,1,:]
-        M2 = pp[2][:,1,1,1,:]
-        @reduce MM[a,b] |= sum(x) M1[x,a]*M2[x,b]
 
-        _, inds = findmax(MM)
-        display(MM)
-        println()
-        println(sum(MM))
-        display(sum(MM, dims = 2))
-        println()
+        @test pp[1] ≈ A1ex.^β
+        @test pp[2] ≈ A2ex.^β
+        @test MM ≈ transpose(C.^β)
 
-        # peps solution
+        # peps solution, first tensor
         Aa1 = pp[1]
         Aa2 = MPO(pp)[2]
 
         @reduce A12[l, u, d, uu, rr, dd, σ] |= sum(x) Aa1[l, u, x, d, σ] * Aa2[x, uu, rr, dd]
         A12 = dropdims(A12, dims=(1,2,3,4,5,6))
-        display(A12)
-        println()
-        println(sum(A12))
         _, spins = findmax(A12)
 
         #solution from the first tensor
         st = get_prop(fg, 1, :spectrum).states
-        println(st)
         @test st[spins] == sol_A1
         @test st[inds[1]] == sol_A1
-        println(spins)
-        println(inds[1])
 
+        # reading projector
         if has_edge(fg, 1, 2)
             p1, en, p2 = get_prop(fg, 1, 2, :split)
         elseif has_edge(fg, 2, 1)
@@ -122,6 +101,7 @@ using TensorCast
             p1 = ones(1,1)
             en = p1
         end
+
         T = pp[2]
         @reduce C[a, b, c, d] := sum(x) p1[$spins, x] * T[x, a, b, c, d]
         _, s = findmax(C[1,1,1,:])
