6ee63d0b1d27962c0d0de41179dc6e54bc0b27a7
kdomino <kdomino@iitis.pl>
2021-02-05 11:47:10 +0100

    add simpler example to testing_probabilities.jl

--- test/testing_probabilities.jl
+++ test/testing_probabilities.jl
@@ -9,6 +9,84 @@ import SpinGlassPEPS: peps_contract
 Random.seed!(1234)
 
 
+Mq = zeros(4,4)
+Mq[1,1] = 1.
+Mq[2,2] = 1.4
+Mq[3,3] = -0.2
+Mq[4,4] = -1.
+
+Mq[1,2] = Mq[2,1] = 0.590
+Mq[1,3] = Mq[3,1] = 0.766
+Mq[2,4] = Mq[4,2] = 0.566
+Mq[3,4] = Mq[4,3] = 0.460
+
+
+@testset "testing marginal/conditional probabilities" begin
+
+    ####   conditional probability implementation
+
+
+    β = 3.
+    g = M2graph(Mq, -1)
+
+    bf = brute_force(g; num_states = 1)
+
+    update_cells!(
+      g,
+      rule = square_lattice((1, 1, 2, 1, 2)),
+    )
+
+    fg = factor_graph(
+        g,
+        energy=energy,
+        spectrum=brute_force,
+    )
+
+    origin = :NW
+    states = collect.(all_states(rank_vec(g)))
+    ρ = exp.(-β .* energy.(states, Ref(g)))
+    Z = sum(ρ)
+
+    println("grid of nodes")
+    display([1; 2])
+    println()
+
+    peps = PepsNetwork(2,1, fg, β, origin)
+    Dcut = 8
+    tol = 0.
+    swep = 4
+    z = peps_contract(peps, Dict{Int, Int}())
+    @test z ≈ Z
+    println(peps)
+
+    boundary_mps = boundaryMPS(peps, Dcut, tol, swep)
+
+    # initialize
+    ps = Partial_sol{Float64}(Int[], 0.)
+
+    #node 1
+    obj1 = conditional_probabs(peps, ps, boundary_mps[1], PEPSRow(peps, 1))
+    _, i = findmax(obj1)
+    ps1 = update_partial_solution(ps, i, obj1[i])
+
+    # test all
+    for i_1 in 1:4
+        @test obj1[i_1] ≈ peps_contract(peps, Dict{Int, Int}(1 => i_1))/z atol = 1e-3
+    end
+
+    obj2 = conditional_probabs(peps, ps1, boundary_mps[2], PEPSRow(peps, 2))
+    obj2 = obj1[i].*obj2
+    _, j = findmax(obj2)
+    ps2 = update_partial_solution(ps1, j, obj2[j])
+
+
+    for i_2 in 1:4
+        @test obj2[i_2] ≈ peps_contract(peps, Dict{Int, Int}(1 => i, 2 =>i_2))/z atol = 1e-3
+    end
+
+end
+
+
 Mq = zeros(9,9)
 Mq[1,1] = 1.
 Mq[2,2] = 1.4
