571b8931cdb904d2937fc9de9f1477388e2b03ee
Konrad Jałowiecki <dexter2206@gmail.com>
2021-02-22 20:59:03 +0100

    Simplify conversion between CuMPS and MPS

--- src/cuda/base.jl
+++ src/cuda/base.jl
@@ -7,38 +7,21 @@ for (T, N) ∈ ((:MPO, 4), (:MPS, 3))
         struct $CuT{T <: Number} <: $AT{T}
             tensors::Vector{CuArray{T, $N}}
         end
-        
+
         $CuT(::Type{T}, L::Int) where {T} = $CuT(Vector{CuArray{T, $N}}(undef, L))
         $CuT(L::Int) = $CuT(Float32, L)
 
         Base.copy(a::$CuT) = $CuT(copy(a.tensors))
 
-        function $T(A::$CuT)
-            T = eltype(A)
-            L = length(A)
-            devA = $T(T, L)
-            for i ∈ eachindex(A)
-                devA[i] = Array(A[i])
-            end
-            devA
-        end
-
-        function $CuT(A::$T)
-            T = eltype(A)
-            L = length(A)
-            cuA = $CuT(T, L)
-            for i ∈ eachindex(A)
-                cuA[i] = cu(A[i])
-            end
-            cuA
-        end
+        $T(A::$CuT) = $T([Array(arr) for arr in A.tensors])
+        $CuT(A::$T) = $CuT([Array(arr) for arr in A.tensors])
     end
 end
 @inline CuMPS(A::AbstractArray) = CuMPS(cu(A), :right)
 @inline CuMPS(A::AbstractArray, s::Symbol, args...) = CuMPS(cu(A), Val(s), typemax(Int), args...)
 @inline CuMPS(A::AbstractArray, s::Symbol, Dcut::Int, args...) = CuMPS(cu(A), Val(s), Dcut, args...)
-@inline CuMPS(A::AbstractArray, ::Val{:right}, Dcut::Int, args...) = _left_sweep_SVD(cu(A), Dcut, args...)
-@inline CuMPS(A::AbstractArray, ::Val{:left}, Dcut::Int, args...) = _right_sweep_SVD(cu(A), Dcut, args...)
+@inline CuMPS(A::AbstractArray, ::Val{:right}, Dcut::Int, args...) = _left_sweep_SVD(CuMPS, cu(A), Dcut, args...)
+@inline CuMPS(A::AbstractArray, ::Val{:left}, Dcut::Int, args...) = _right_sweep_SVD(CuMPS, cu(A), Dcut, args...)
 
 function CUDA.randn(::Type{CuMPS{T}}, L::Int, D::Int, d::Int) where {T}
     ψ = CuMPS(L)
@@ -51,7 +34,7 @@ function CUDA.randn(::Type{CuMPS{T}}, L::Int, D::Int, d::Int) where {T}
 end
 
 function CUDA.randn(::Type{CuMPO{T}}, L::Int, D::Int, d::Int) where {T}
-    ψ = CUDA.randn(CuMPS{T}, L, D, d^2) 
+    ψ = CUDA.randn(CuMPS{T}, L, D, d^2)
     CuMPO(ψ)
 end
 
@@ -60,7 +43,7 @@ function CuMPS(vec::CuVector{<:Number})
     ψ = CuMPS(L)
     for i ∈ 1:L
         ψ[i] = reshape(copy(vec[i]), 1, :, 1)
-    end    
+    end
 end
 
 function CuMPO(ψ::CuMPS)
@@ -71,12 +54,12 @@ function CuMPO(ψ::CuMPS)
     for i ∈ 1:L
         A = ψ[i]
         d = isqrt(size(A, 2))
-        
-        @cast W[x, σ, y, η] |= A[x, (σ, η), y] (σ:d) 
+
+        @cast W[x, σ, y, η] |= A[x, (σ, η), y] (σ:d)
         O[i] = W
-    end 
+    end
     O
-end 
+end
 
 function CuMPS(O::CuMPO)
     L = length(O)
@@ -85,8 +68,8 @@ function CuMPS(O::CuMPO)
     for i ∈ 1:L
         W = O[i]
         @cast A[x, (σ, η), y] := W[x, σ, y, η]
-        ψ[i] = A     
-    end 
+        ψ[i] = A
+    end
     return ψ
 end
 
@@ -94,71 +77,23 @@ function is_left_normalized(ψ::CuMPS)
     for i ∈ eachindex(ψ)
         A = ψ[i]
         DD = size(A, 3)
-    
+
         @cutensor Id[x, y] := conj(A[α, σ, x]) * A[α, σ, y] order = (α, σ)
         if norm(cu(I(DD)) - Id) > 1e-6 return false end
-    end  
+    end
     true
 end
 
-function is_right_normalized(ϕ::CuMPS)   
+function is_right_normalized(ϕ::CuMPS)
     for i ∈ eachindex(ϕ)
         B = ϕ[i]
         DD = size(B, 1)
 
         @cutensor Id[x, y] := B[x, σ, α] * conj(B[y, σ, α]) order = (α, σ)
         if norm(cu(I(DD)) - Id) > 1e-6 return false end
-    end 
+    end
     true
 end
-
-function _right_sweep_SVD(Θ::CuArray{T}, Dcut::Int=typemax(Int), args...) where {T}
-    rank = ndims(Θ)
-    ψ = CuMPS(T, rank)
-
-    V = reshape(copy(conj(Θ)), (length(Θ), 1))
-
-    for i ∈ 1:rank
-        d = size(Θ, i)
-
-        # reshape
-        VV = conj.(transpose(V)) # hack @cast does not work with Adjoint
-        @cast M[(x, σ), y] |= VV[x, (σ, y)] (σ:d)
-       
-        # decompose
-        U, Σ, V = _truncate_svd(CUDA.svd(M), Dcut)
-        V *= Diagonal(Σ)
-
-        # create MPS  
-        @cast A[x, σ, y] |= U[(x, σ), y] (σ:d)
-        ψ[i] = A
-    end
-    ψ
-end
-
-function _left_sweep_SVD(Θ::CuArray{T}, Dcut::Int=typemax(Int), args...) where {T}
-    rank = ndims(Θ)
-    ψ = CuMPS(T, rank)
-
-    U = reshape(copy(Θ), (length(Θ), 1))
-
-    for i ∈ rank:-1:1
-        d = size(Θ, i)
-
-        # reshape
-        @cast M[x, (σ, y)] |= U[(x, σ), y] (σ:d)
-
-        # decompose
-        U, Σ, V = _truncate_svd(CUDA.svd(M), Dcut)
-        U *= Diagonal(Σ)
-
-        # create MPS  
-        VV = conj.(transpose(V)) # hack @cast does not work with Adjoint
-        @cast B[x, σ, y] |= VV[x, (σ, y)] (σ:d)
-        ψ[i] = B
-    end
-    ψ
-end 
 
 function _truncate_svd(F::CuSVD, Dcut::Int)
     U, Σ, V = F
