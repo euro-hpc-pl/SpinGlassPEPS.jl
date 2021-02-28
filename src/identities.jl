fb4714cf381bde85d2add1866b7c14fddc52ab37
Łukasz Pawela <lukasz.pawela@gmail.com>
2021-02-28 15:46:27 +0100

    correctly choose types of indexed identities

--- src/identities.jl
+++ src/identities.jl
@@ -1,13 +1,28 @@
 export IdentityMPO, IdentityMPS
-struct IdentityMPS{T <: Number} <: AbstractMPS{T} end
-struct IdentityMPO{T <: Number} <: AbstractMPO{T} end
-IdentityMPS() = IdentityMPS{Float64}()
-IdentityMPO() = IdentityMPO{Float64}()
+struct IdentityMPS{T <: Number, S <: AbstractArray} <: AbstractMPS{T} end
+struct IdentityMPO{T <: Number, S <: AbstractArray} <: AbstractMPO{T} end
+IdentityMPS() = IdentityMPS{Float64, Array}()
+IdentityMPO() = IdentityMPO{Float64, Array}()
+
+IdentityMPS(::Type{T}) where {T <: AbstractArray} = IdentityMPS{Float64, T}
+IdentityMPO(::Type{T}) where {T <: AbstractArray} = IdentityMPO{Float64, T}
+
+IdentityMPS(::Type{S}, ::Type{T}) where {S <: Number, T <: AbstractArray} = IdentityMPS{S, T}
+IdentityMPO(::Type{S}, ::Type{T}) where {S <: Number, T <: AbstractArray} = IdentityMPO{S, T}
 
 const IdentityMPSorMPO = Union{IdentityMPO, IdentityMPS}
 
-@inline Base.getindex(::IdentityMPS{T}, ::Int) where {T} = ones(T, 1, 1, 1)
-@inline Base.getindex(::IdentityMPO{T}, ::Int) where {T} = ones(T, 1, 1, 1, 1)
+@inline function Base.getindex(::IdentityMPS{S, T}, ::Int) where {S, T}
+    ret = similar(T{S}, (1, 1, 1))
+    ret[1] = one(S)
+    ret
+end
+
+@inline function Base.getindex(::IdentityMPO{S, T}, ::Int) where {S, T}
+    ret = similar(T{S}, (1, 1, 1, 1))
+    ret[1] = one(S)
+    ret
+end
 
 LinearAlgebra.dot(O::AbstractMPO, ::IdentityMPO) = O
 LinearAlgebra.dot(::IdentityMPO, O::AbstractMPO) = O
@@ -28,7 +43,7 @@ end
 LinearAlgebra.dot(::IdentityMPO, ψ::AbstractMPS) = ψ
 LinearAlgebra.dot(ψ::AbstractMPS, ::IdentityMPO) = ψ
 
-function Base.show(io::IO, ψ::IdentityMPSorMPO)
+function Base.show(io::IO, ::IdentityMPSorMPO)
     println(io, "Trivial matrix product state")
     println(io, "   ")
 end
