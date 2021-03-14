511c5cc93e468e33a4e0dc95c80ba209f1fbdbb3
Łukasz Pawela <lukasz.pawela@gmail.com>
2021-03-14 01:33:04 +0100

    restore cuda tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,18 @@
 using Test, SpinGlassPEPS
+
+my_tests = []
+if CUDA.functional() && CUDA.has_cutensor() && false
+    CUDA.allowscalar(false)
+    include("cuda/test_helpers.jl")
+    push!(
+        my_tests,
+        "cuda/base.jl",
+        "cuda/contractions.jl",
+        "cuda/compressions.jl",
+        "cuda/spectrum.jl"
+    )
+end
+
+for my_test in my_tests
+    include(my_test)
+end
