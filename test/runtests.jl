ca5674a71a0c3144f8762c43674eed985e721a80
Łukasz Pawela <3093117+lpawela@users.noreply.github.com>
2021-03-19 02:19:15 +0100

    Seperate repo into subpackages (#75)
    * remove fiels moved to other packages
    
    * change License to apache
    
    * dummy runtests
    
    * restore cuda tests
    
    * cleanup dependencies
    
    * fix dependencies and reexporting

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,4 @@
-using CUDA
-using SpinGlassPEPS
-using LinearAlgebra
-using TensorOperations
-using TensorCast
-using LightGraphs
-using MetaGraphs
-using Random
-using Logging
-using Statistics
-using NPZ
-
-disable_logging(LogLevel(1))
-
-using Test
-
-include("test_helpers.jl")
+using Test, CUDA, SpinGlassPEPS
 
 my_tests = []
 if CUDA.functional() && CUDA.has_cutensor() && false
@@ -28,23 +12,6 @@ if CUDA.functional() && CUDA.has_cutensor() && false
         "cuda/spectrum.jl"
     )
 end
-
-my_tests = []
-include("test_helpers.jl")
-push!(my_tests,
-      "base.jl",
-      "utils.jl",
-      "contractions.jl",
-      "compressions.jl",
-      "identities.jl",
-      "ising.jl",
-      "MPS_search.jl",
-      "factor.jl",
-      "PEPS.jl",
-      "contract.jl",
-      "search.jl",
-      "search_2.jl"
-)
 
 for my_test in my_tests
     include(my_test)
